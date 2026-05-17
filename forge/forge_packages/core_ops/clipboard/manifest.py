3b98f36b6add3f52f6c23200dab8d4f2c8a6d3ec
jackatttack <69674355+jackatttack@users.noreply.github.com>
2026-05-17 01:09:16 +0100

    Publish Forge release stage

---
+++ forge/forge_packages/core_ops/clipboard/manifest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+MANIFEST = {
+    'name': 'clipboard',
+    'version': '0.1.0',
+    'op': 'CLIPBOARD',
+    'kind': 'core-op',
+    'summary': 'CLIPBOARD copies a project-relative text file to the iOS clipboard.',
+}
