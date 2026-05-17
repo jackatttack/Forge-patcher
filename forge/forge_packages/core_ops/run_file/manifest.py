6bfb24eb29437a0ffba9885e0f6f98683c1c1e43
jackatttack <69674355+jackatttack@users.noreply.github.com>
2026-05-17 01:12:50 +0100

    Publish final Forge core ops

---
+++ forge/forge_packages/core_ops/run_file/manifest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+MANIFEST = {
+    'name': 'run_file',
+    'version': '0.1.0',
+    'op': 'RUN_FILE',
+    'kind': 'core-op',
+    'summary': 'RUN_FILE executes a Python file under the project root and captures stdout/stderr.',
+}
