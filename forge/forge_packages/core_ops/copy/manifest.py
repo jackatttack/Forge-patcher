1ff69e81a6a3bd217e98d359a1703e773f02f1f4
jackatttack <69674355+jackatttack@users.noreply.github.com>
2026-05-17 01:09:20 +0100

    Publish Forge release stage

---
+++ forge/forge_packages/core_ops/copy/manifest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+MANIFEST = {
+    'name': 'copy',
+    'version': '0.1.0',
+    'op': 'COPY',
+    'kind': 'core-op',
+    'summary': 'COPY copies a project-relative file and records reversible destination metadata.',
+}
