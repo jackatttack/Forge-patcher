26d560d5d8a99e95ca61f9399036dd690f248ae5
jackatttack <69674355+jackatttack@users.noreply.github.com>
2026-05-17 13:52:39 +0100

    Publish PIP core op

---
+++ forge/forge_packages/core_ops/pip/manifest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+MANIFEST = {
+    'name': 'pip',
+    'version': '0.1.0',
+    'op': 'PIP',
+    'kind': 'core-op',
+    'summary': 'Install or uninstall pure-Python packages from PyPI in Pythonista.',
+    'risk': 'mutating',
+    'domains': ['packages', 'pythonista', 'environment'],
+}
