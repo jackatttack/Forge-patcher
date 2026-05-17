247fa44b0da3c2799e9e29f7aa9010428f630c48
jackatttack <69674355+jackatttack@users.noreply.github.com>
2026-05-17 01:11:48 +0100

    Publish remaining Forge core ops

---
+++ forge/forge_packages/core_ops/move/manifest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+MANIFEST = {
+    'name': 'move',
+    'version': '0.1.0',
+    'op': 'MOVE',
+    'kind': 'core-op',
+    'summary': 'MOVE moves a project-relative file and records reversible source/destination metadata.',
+}
